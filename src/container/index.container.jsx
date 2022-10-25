642ef9e65bb23a861daec3e83321673dfdfcbc62
St-Pardon <53058358+St-Pardon@users.noreply.github.com>
2022-10-25 18:09:44 +0100

    saving progress

--- src/container/index.container.jsx
+++ src/container/index.container.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
+import { RiShareForwardLine } from 'react-icons/ri';
 import ListItems from '../component/listItem/list';
-import { Avi, Main, Section, SlackName, TwitterName } from './index.styled';
+import {
+  Avi,
+  Main,
+  Section,
+  SlackName,
+  Share,
+  TwitterName,
+} from './index.styled';
 import { apidata } from '../assets/apidata';
 
 const LandingPage = () => {
   return (
     <Main>
+      <Share>
+        <RiShareForwardLine />
+      </Share>
       <Section>
-        <Avi id='profile_img' src='https://images.nappy.co/uploads/large/20200905-doryus-4258-2001599358408fhrsfpc68vagsgynjlikyxocgmqxjlikvpdzftm5blnucjtvtljcckopnawo9dtpfhlhyjkazcjybwnal8dd8dijj5ocsvyyxhzn.jpg?auto=format&w=1280&q=75' />
-        <TwitterName id='twitter'>Onyedikachi</TwitterName>
-        <SlackName id='slack'>Onyedikachi Onu</SlackName>
+        <Avi
+          id="profile_img"
+          src="https://images.nappy.co/uploads/large/20200905-doryus-4258-2001599358408fhrsfpc68vagsgynjlikyxocgmqxjlikvpdzftm5blnucjtvtljcckopnawo9dtpfhlhyjkazcjybwnal8dd8dijj5ocsvyyxhzn.jpg?auto=format&w=1280&q=75"
+        />
+        <TwitterName id="twitter">Onyedikachi</TwitterName>
+        <SlackName id="slack">Onyedikachi Onu</SlackName>
       </Section>
       <Section>
         {apidata.map((item, i) => (
--- src/container/index.styled.jsx
+++ src/container/index.styled.jsx
@@ -3,10 +3,11 @@ import styled from 'styled-components';
 export const Main = styled.main`
   max-width: 800px;
   width: 100%;
-  margin: 0 auto 100px auto;
+  margin: 50px auto 100px auto;
   display: flex;
   flex-direction: column;
   gap: 50px;
+  position: relative;
 `;
 
 export const Section = styled.section`
@@ -23,9 +24,11 @@ export const TwitterName = styled.h1`
   color: #101828;
   text-align: center;
 `;
+
 export const SlackName = styled.h3`
   display: none;
 `;
+
 export const Avi = styled.img`
   width: 100px;
   height: 100px;
@@ -34,3 +37,6 @@ export const Avi = styled.img`
   border-radius: 100%;
 `;
 
+export const Share = styled.div`
+  position: absolute;
+`;
