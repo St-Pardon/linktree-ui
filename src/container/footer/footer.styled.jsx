f841ec9cd166589a32de6e39f0d03e8ea9f7f713
St-Pardon <53058358+St-Pardon@users.noreply.github.com>
2022-10-26 20:52:21 +0100

    :wrench: FIX footer

--- src/container/footer/footer.styled.jsx
+++ src/container/footer/footer.styled.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { devices } from '../../styles/breakpoints';
 
 export const FooterContainer = styled.footer`
   width: 100%;
@@ -6,6 +7,9 @@ export const FooterContainer = styled.footer`
   justify-content: space-around;
   align-items: center;
   padding: 20px 0;
+  @media ${devices.mobileL}{
+    flex-wrap: wrap;
+  }
 `;
 
 export const Zuri = styled.h3`
@@ -14,6 +18,10 @@ export const Zuri = styled.h3`
   font-weight: 700;
   display: flex;
   gap: 5px;
+  @media ${devices.mobileL}{
+    font-size: 1rem;
+    gap: 3px;
+  }
 `;
 
 export const Hng = styled.h3`
@@ -22,10 +30,16 @@ export const Hng = styled.h3`
   font-family: inter;
   font-weight: 400;
   line-height: 24px;
+  @media ${devices.mobileL}{
+    font-size: 0.7rem;
+  }
 `;
 
 export const IngressLogo = styled.img`
-  width: 132px;`;
+  width: 132px;
+  @media ${devices.mobileL}{
+    width: 70px;
+  }`;
 
 export const Bull = styled.span`
   color: #ff0000;
